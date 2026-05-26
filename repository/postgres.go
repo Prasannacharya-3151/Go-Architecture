47cba3ad6b8979d226348f73f056483c6ed4fe51
Prasannacharya-3151 <prasannacharya428@gmail.com>
2026-05-26 23:05:53 +0530

    updated a repo

--- repository/postgres.go
+++ repository/postgres.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"database/sql" //manages the db connection like Exec,Query,QuryRow
 	"fmt"
+
+	"github.com/Prasannacharya-3151/Go-Bank-Service/db/models"
 	"github.com/Prasannacharya-3151/Go-Bank-Service/models"
 )
 
@@ -28,5 +30,61 @@ func (r *PostgresRepository) Init() error{ //init is a method of the PostgresRep
 }
 
 func (r *PostgresRepository) CreateCustomer(customer *models.Customer) error{
-	query := 
+	query := `INSERT INTO customers (id,name,email,phone,address) VALUES  ($1,$2,$3,$4,$5)`
+	_,err := r.db.Exec(query,customer.ID,customer.Name,customer.Email,customer.Phone,customer.Address)
+	return err
 }
+
+func (r *PostgresRepository) GetCustomer(id string) (*models.Customer,error){
+	query:= `SELECT id,name,email,phone,address FROM customers WHERE id = $1`
+	var customer models.Customer
+	err := r.db.QueryRow(query,id).Scan(
+		&customer.ID,
+		&customer.Name,
+		&customer.Email,
+		&customer.Phone,
+		&customer.Address,
+	)
+
+	if err!=nil{
+		return nil,err
+	}
+
+	func (r *PostgresRepository) UpdateCustomer(customer *models.Customer) error {
+		query := `UPDATE customers SET name=$1, email=$2, phone=$3, address=$4, WHERE id = $5`
+		_,err := r.db.Exec(query,customer.Name,customer.Email,customer.Phone,customer.Address,customer.ID)
+		return err
+	}
+
+	func (r *PostgresRepository) DeleteCustomer(id string) error{
+		query:= `DELETE FROM customers WHERE id = $1`
+		result,err:= r.db.Exec(query, id)
+		if err!=nill {
+			return fmt.Errorf(
+				rowsAffected,err := result.RowsAffected()
+
+				if rowsAffected == 0 {
+					return fmt.Errorf("task with ID %s not found", id)
+				}
+				return err
+		}
+
+		func (r *PostgresRepository) ListCustomers()([]*models.Customer,error){
+			query := `SELECT * FROM customers`
+			rows,err := r.db.Query(query)
+			if err!=nil{
+				return nil, err
+			}
+			var customers []*models.Customer
+
+			for rows.Next(){
+				var customer models.Customererr
+				if err := rows.Scan(&customer.ID,&customer.Name,&customer.Email,&customer.Phone.&customer.Address); err!=nil{
+					return nil, err
+				}
+				customer = append(customers, &customer)
+
+			}
+
+			return customers, nil
+		}
